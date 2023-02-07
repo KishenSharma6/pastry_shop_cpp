8ebe6f0a9066c12aae1271da67fe546d0b025fe0
KishenSharma6 <ksharma6@proton.me>
2023-02-07 15:13:54 -0800

    Updated test functions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <list>
+#include <string>
+
 
 #include "include/menu.h"
+#include "include/inventory.h"
 
 int main(){
-    Frontend test;
+    //Frontend test;
+    //test.welcome_menu();
+    std::list<std::string> food;
+    std::list<std::string> drink;
+    
+    food.push_back("apples");
+    food.push_back("pies");
 
-    test.welcome_menu();
+    Inventory test(food, drink);
+    test.write_inventory();
     return 0;
 }
