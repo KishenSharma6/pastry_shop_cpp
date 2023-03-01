cf58b33a6807cbbb9ab5d1bb68d66bf2a1fa932f
KishenSharma6 <ksharma6@proton.me>
2023-03-01 09:40:50 -0800

    Completed Inventory update_count()

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -14,6 +14,9 @@ Inventory::Inventory(){}
 void Inventory::view_inventory()
     { /*Display current inventory to user*/
         std::fstream inventory_file;
+        
+        std::cout << "Here is the current inventory:\n" << std::endl;
+        
         inventory_file.open("/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt", std::ios::in);
 
         if (inventory_file.is_open())
@@ -32,12 +35,90 @@ void Inventory::view_inventory()
 void Inventory::update_count()
     {
         //function allows you to modify the current inventory quantitry 
-        ;
+
+        
+        bool continue_operation = true;
+
+        do
+        {
+            std::string target_item = "";
+            std::string updated_name = "";
+            std::string updated_quant = "";
+            std::string updated_price = "";
+
+            std::cout << "Which inventory item would you like to update?" << std::endl;
+            std::cin >> std::ws;
+            getline(std::cin,target_item);
+
+            std::cout << "What is the correct name of " << target_item << "(s) in stock?" << std::endl;
+            std::cin >> std::ws;
+            getline(std::cin, updated_name);
+
+            std::cout << "What is the correct number of " << updated_name << "(s) in stock?" << std::endl;
+            std::cin >> updated_quant;
+
+            std::cout << "What is the correct price of each " << updated_name << "(s) in stock?" << std::endl;
+            std::cin >> updated_price;
+
+            std::ifstream curr_inventory;
+            std::ofstream new_inventory;
+
+            curr_inventory.open("/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
+            new_inventory.open("/home/kishen/documents/c++_projects/pastry_shop/data/temp.txt");
+
+
+            std::string line;
+            while (getline(curr_inventory, line))
+            {
+                if (line.rfind(target_item, 0) == 0)
+                {
+                    new_inventory << updated_name << "," << updated_quant << "," << updated_price << std::endl;
+                }
+                else
+                {
+                    new_inventory << line << std::endl;
+                }
+            }
+
+            new_inventory.close();
+            curr_inventory.close();
+
+            remove("/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
+            rename("/home/kishen/documents/c++_projects/pastry_shop/data/temp.txt", "/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
+        
+
+            
+
+            std::string user_input;
+
+            std::cout << "Would you like to update an additional item in inventory(Y/N)?" << std::endl;
+            std::cin >> user_input;
+
+            transform(user_input.begin(), user_input.end(), user_input.begin(), ::toupper);
+
+            if ((user_input) == "Y")
+            {
+                ;
+            }
+            else if (user_input == "N")
+            {
+                continue_operation = false;
+            }
+            else
+            {
+                throw std::invalid_argument("Must enter Y or N, please try again");
+            }
+        }
+        while (continue_operation);    
+        
+        
+
+
     }
 
 void Inventory::add_item()
     {
-        
+        // add new item line to inventory.txt. Collects name, quant, price and saves to inventory.txt
         bool additional_item = true;
 
         while (additional_item)
@@ -80,6 +161,7 @@ void Inventory::add_item()
 
 void Inventory::remove_item()
     {
+        // remove item line in inventory.txt. NOTE matches on beginning of item name string
         std::string target;
 
         std::cout << "Which item would you like removed from inventory?" << std::endl;
@@ -108,9 +190,9 @@ void Inventory::remove_item()
         remove("/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
         rename("/home/kishen/documents/c++_projects/pastry_shop/data/temp.txt", "/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
         
-        // rename("/home/kishen/documents/c++_projects/pastry_shop/data/temp.txt", "/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
-        // remove("/home/kishen/documents/c++_projects/pastry_shop/data/inventory.txt");
+
     }
+
 void write_inventory(std::string item, int quantity, float price)
     {
         std::fstream inventory_file;
