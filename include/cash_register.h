4319661d650decf45463fb9905263e25e8203f7a
KishenSharma6 <ksharma6@proton.me>
2023-03-10 13:58:25 -0800

    added verify_register, write_register functions to cash register class

--- include/cash_register.h
+++ include/cash_register.h
@@ -13,9 +13,9 @@ private:
     
 public:
     Cash_Register();
-    bool verify_register(Cash_Register);
-    void write_register(Cash_Register, std::string path_to_data);
-    void read_register(Cash_Register, std::string path_to_data);
+    bool verify_register(const Cash_Register& cash_reg);
+    int write_register(const Cash_Register& cash_reg, std::string path_to_data);
+    int read_register(Cash_Register, std::string path_to_data);
     void update_register();
     
 
--- src/cash_register.cpp
+++ src/cash_register.cpp
@@ -28,20 +28,49 @@ Cash_Register::Cash_Register()
                 
 };
 
-bool Cash_Register::verify_register(Cash_Register)
+bool Cash_Register::verify_register(const Cash_Register& cash_reg)
 {
+    char user_input;
+
+    std::cout << "Please confirm that the contents of the register are correct: \n" 
+                 "Number of TWENTIES: " + std::to_string(cash_reg.twenties) + "\n" 
+                 "Number of TENS: " + std::to_string(cash_reg.tens) + "\n" 
+                 "Number of FIVES: " + std::to_string(cash_reg.fives) + "\n" 
+                 "Number of ONES: " + std::to_string(cash_reg.ones) + "\n" << std::endl;
+    std::cout << "Is this correct?(Y/N)" << std::endl;
+    std::cin >> user_input;
+
+    if (std::toupper(user_input) == 'Y')
+    {
+        return true;
+    } else if (std::toupper(user_input) == 'N')
+    {
+        return false;
+    } else
+    {
+        throw std::invalid_argument( "received incorrect value" );
+    }
+    
+};
+
+int Cash_Register::write_register(const Cash_Register& cash_reg, std::string path_to_data)
+{
+    std::ofstream register_data(path_to_data);
+    register_data << cash_reg.twenties << "," << cash_reg.tens << "," << cash_reg.fives << ","<<
+                     cash_reg.ones << std::endl;
+
+    register_data.close();
+    return 0;
 
 };
 
-void Cash_Register::write_register(Cash_Register, std::string path_to_data)
+
+int Cash_Register::read_register(Cash_Register, std::string path_to_data)
 {
+    std::ifstream register_data(path_to_data);
 
-};
 
-
-void Cash_Register::read_register(Cash_Register, std::string path_to_data)
-{
-
+    return 0;
 };
 
 void Cash_Register::update_register()
